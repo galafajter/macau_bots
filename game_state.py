825ca3fd16d019cd0d82c52b7d02a800f73a5d6a
Robert Dubiela <s196506@student.pg.edu.pl>
2026-03-11 21:22:01 +0100

    Add: `reset_active_effect(self)` to handle queen event

--- game_state.py
+++ game_state.py
@@ -6,13 +6,10 @@ from dataclasses import dataclass
 
 @dataclass
 class GameState:
-    
 
     deck: Deck
     players: List[Player]
     current_player_index: int
-
-    playable_cards: List[Card]
 
     active_effect: Effect | None = None
 
@@ -20,4 +17,14 @@ class GameState:
     demanded_value: Value | None = None
 
     cards_to_draw: int = 0
-    turns_to_skip: int = 0
+    turns_being_blocked: int = 0
+
+    turn_direction: int = 1
+
+    def reset_active_effect(self):
+        self.cards_to_draw = 0
+        self.turns_being_blocked = 0
+        self.demanded_suit = None
+        self.demanded_value = None
+
+
