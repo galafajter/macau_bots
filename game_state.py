f8eeac7e51281d96c87257bb017bafe806fa344e
Robert Dubiela <s196506@student.pg.edu.pl>
2026-03-16 17:56:54 +0100

    Fix: Basic changes and refactoring

--- game_state.py
+++ game_state.py
@@ -11,20 +11,29 @@ class GameState:
     players: List[Player]
     current_player_index: int
 
-    active_effect: Effect | None = None
-
     demanded_suit: Suit | None = None
     demanded_value: Value | None = None
+    demand_turns_left: int = 0
 
     cards_to_draw: int = 0
-    turns_being_blocked: int = 0
+
+    blocked_turns_per_player: dict[int, int] = field(default_factory=dict)
+    block_count: int = 0
+
+    effect_active: bool = False
+    execute_effect: bool = False
 
     turn_direction: int = 1
 
+    skip_turn: bool = False
+
     def reset_active_effect(self):
         self.cards_to_draw = 0
-        self.turns_being_blocked = 0
+        self.block_count = 0
         self.demanded_suit = None
         self.demanded_value = None
 
+    @property
+    def current_player(self) -> Player:
+        return self.players[self.current_player_index]
 
--- macau_rules.py
+++ macau_rules.py
@@ -1,4 +1,5 @@
-from makao_game import Card, Value, Suit
+from card import Card, Value, Suit
+from player import Player
 from typing import List
 
 class Rules:
@@ -11,7 +12,7 @@ class Rules:
         self.nonfunctional_cards: List[Card] = [Card(s, v, effect=None) for s in Suit for v in [Value.FIVE, Value.SIX,
                                                                                                 Value.SEVEN, Value.EIGHT,
                                                                                                 Value.NINE, Value.TEN]]
-        self.all_cards = self.playable_cards + self.nonfunctional_cards
+        self.all_cards = self.functional_cards + self.nonfunctional_cards
 
         self.ruleset = []
 
@@ -22,35 +23,6 @@ class Rules:
         self.king_war_flag = False
         self.suit_demand_flag = False
 
-    def get_game_state(self, top_card: Card):
-        if top_card.value in [Value.TWO, Value.THREE]:
-            # self.get_two_three_playable()
-            self.war_flag = True
-        elif top_card.value  == Value.FOUR:
-            # self.get_four_playable()
-            self.block_flag = True
-        elif top_card.value == Value.JACK:
-            # self.get_jack_playable(optional_demand)
-            self.value_demand_flag = True
-        elif top_card.value == Value.QUEEN:
-            # self.get_queen_playable()
-            self.queen_flag = True
-        elif top_card.value == Value.KING:
-            # self.get_king_playable()
-            self.king_war_flag = True
-        elif top_card.value == Value.ACE:
-            # self.get_ace_playable(optional_demand)
-            self.suit_demand_flag = True
-
-        return {
-            "war": self.war_flag,
-            "block": self.block_flag,
-            "value_demand": self.value_demand_flag,
-            "skip": self.queen_flag,
-            "king_war": self.king_war_flag,
-            "suit_demand": self.suit_demand_flag,
-            # "playable_cards": self.playable_cards
-        }
 
     def get_two_three_playable(self):
         self.playable_cards = [card for card in self.functional_cards if card.value in [Value.TWO, Value.THREE]]
@@ -67,15 +39,24 @@ class Rules:
     def get_king_playable(self):
         self.playable_cards = [card for card in self.functional_cards if card.value == Value.KING]
 
+    # TODO or Value.ACE
     def get_ace_playable(self, demanded_suit):
         self.playable_cards = [card for card in self.all_cards if card.suit == demanded_suit]
-        # TODO or Value.ACE
+
+    def get_nonfunctional_playable(self, top_card):
+        self.playable_cards = [card for card in self.all_cards if (card.value == top_card.value) \
+                               or (card.suit == top_card.suit) or (card.value == Value.QUEEN)]
 
 class GameMaster:
     def __init__(self):
         self.rules = Rules()
         self.cards_to_draw = 0
         self.turns_to_wait = 0
+        self.evaluated_game_state = {}
+        self.current_player: Player | None = None
+
+    def set_current_player(self, player: Player):
+        self.current_player = player
 
     def evaluate_game_state(self, top_card: Card):
         game_state = self.rules.get_game_state(top_card)
@@ -83,22 +64,41 @@ class GameMaster:
             self.cards_to_draw += top_card.value.value
             self.rules.get_two_three_playable()
         elif game_state["block"]:
-            self.turns_to_wait += 1
+            self.turns_to_wait += 1 # plus one waiting turn for one card
             self.rules.get_four_playable()
         elif game_state["value_demand"]:
-            ...
+            value_demand = self.current_player.make_value_demand()
+            self.evaluate_demand("value", value_demand)
         elif game_state["skip"]:
             ...
             self.rules.get_queen_playable()
         elif game_state["king_war"]:
-            self.cards_to_draw += 5
+            # TODO add war ending with clubs and diamonds and reversing with spades
+            self.cards_to_draw += 5 # TODO think how to avoid magic numbers
             self.rules.get_king_playable()
         elif game_state["suit_demand"]:
-            ...
+            suit_demand = self.current_player.make_suit_demand()
+            self.evaluate_demand("suit", suit_demand)
         else:
             # normal card was played
-            # self.rules.get_nonfunctional_playable(top_card)
-            ...
+            self.rules.get_nonfunctional_playable(top_card)
+
+        return {
+            "war": self.rules.war_flag,
+            "block": self.rules.block_flag,
+            "value_demand": self.rules.value_demand_flag,
+            "skip": self.rules.queen_flag,
+            "king_war": self.rules.king_war_flag,
+            "suit_demand": self.rules.suit_demand_flag,
+            "playable_cards": self.rules.playable_cards,
+            "cards_to_draw": self.cards_to_draw,
+            "turns_to_wait": self.turns_to_wait,
+        }
+
+    # TODO how to implement function of block and card drawing
+
+    def reset_war(self):
+        ...
 
     def evaluate_demand(self, demand_type: str, demand: Suit | Value):
         if demand_type == "value":
