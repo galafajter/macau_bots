7ea031356f59d87ffa29686971cc4a10afb0bccc
galafajter <rdubiela@gmail.com>
2026-04-04 21:37:43 +0200

    Add: `RandomPlayer` bot

--- player.py
+++ player.py
@@ -51,3 +51,47 @@ class Player:
             return card
         else:
             return None
+        
+
+class RandomPlayer(Player):
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def __str__(self):
+        return f"Player: {self.name}"
+
+    def __repr__(self):
+        return f"Player: {self.name}"
+    
+    def play_card(self, card: Card) -> Optional[Card]:
+        if card in self.hand:
+            self.hand.remove(card)
+            return card
+        else:
+            return None
+
+    def draw_card(self, card: Card):
+        self.hand.append(card)
+
+    def evaluate_hand_for_playable_cards(self, playable_cards: List[Card]):
+        """Choose cards that player can throw on top_stack_card"""
+        self.playable_hand = []
+
+        for card in self.hand:
+            if card in playable_cards:
+                self.playable_hand.append(card)
+
+
+    def make_value_demand(self) -> Value:
+        return random.choice(list(Value))
+
+    def make_suit_demand(self) -> Suit:
+        return random.choice(list(Suit))
+    
+
+    def make_move(self) -> Card:
+
+        if not self.playable_hand:
+            return None
+        
+        return self.play_card(random.choice(self.playable_hand))
