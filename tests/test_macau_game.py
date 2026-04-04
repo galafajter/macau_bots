ce3105a700cb4589f32bc73aec3a3751b2fcb0d3
galafajter <rdubiela@gmail.com>
2026-04-04 21:43:26 +0200

    Refactor: Improve test by using pytest builtins

--- tests/test_macau_game.py
+++ tests/test_macau_game.py
@@ -1,10 +1,12 @@
 from makao_game import MacauGame
 import random
+import pytest
 
-def test_game_always_ends():
-    for seed in range(100):
-        random.seed(seed)
-        game = MacauGame(players_num=3)
-        res = game.play()
-        assert res != "error"
-        assert res is not None
+
+@pytest.mark.parametrize("seed", range(1000))
+def test_game_always_ends(seed):
+    random.seed(seed)
+    game = MacauGame(players_num=3)
+    res = game.play()
+    assert res != "error"
+    assert res is not None
