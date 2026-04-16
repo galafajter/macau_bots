b074e7934fc5fc645e2347a9343d25eea1e42798
Robert Dubiela <s196506@student.pg.edu.pl>
2026-04-16 22:59:10 +0200

    Refactor: Made `Player` an abstract class and deleted redundant methods from rest of the classes

--- player.py
+++ player.py
@@ -1,19 +1,21 @@
+from abc import abstractmethod, ABC
+
 from card import Card, Value, Suit
 from typing import List, Optional
 import random
 
 
-class Player:
+class Player(ABC):
     def __init__(self, name: str):
         self.name = name
         self.hand: List[Card] = []
         self.playable_hand: List[Card] = []
 
     def __str__(self):
-        return f"Player: {self.name}"
+        return f"{self.__class__.__name__}: {self.name}"
 
     def __repr__(self):
-        return f"Player: {self.name}"
+        return f"{self.__class__.__name__}: {self.name}"
 
     def draw_card(self, card: Card):
         self.hand.append(card)
@@ -26,23 +28,13 @@ class Player:
             if card in playable_cards:
                 self.playable_hand.append(card)
 
+    @staticmethod
+    def make_value_demand() -> Value:
+        return random.choice([Value.FIVE, Value.SIX, Value.SEVEN, Value.EIGHT, Value.NINE, Value.TEN])
 
-    def make_value_demand(self) -> Value:
-        return random.choice(list(Value))
-
-    def make_suit_demand(self) -> Suit:
+    @staticmethod
+    def make_suit_demand() -> Suit:
         return random.choice(list(Suit))
-
-
-    def make_move(self) -> Optional[Card]:
-        """
-        Method that implements strategy of a player.
-
-        Currently, it is basic strategy - throw first fitting card that you see.
-        """
-        if not self.playable_hand:
-            return None
-        return self.play_card(self.playable_hand[0])
 
     def play_card(self, card: Card) -> Optional[Card]:
         if card in self.hand:
@@ -50,7 +42,16 @@ class Player:
             return card
         else:
             return None
-        
+
+    @abstractmethod
+    def make_move(self) -> Optional[Card]:
+        """
+        Method that implements strategy of a player.
+
+        Currently, it is basic strategy - throw first fitting card that you see.
+        """
+        raise NotImplementedError
+
 
 class RandomPlayer(Player):
     """
@@ -58,38 +59,7 @@ class RandomPlayer(Player):
     """
     def __init__(self, name):
         super().__init__(name)
-    
-    def __str__(self):
-        return f"Random player: {self.name}"
 
-    def __repr__(self):
-        return f"Random player: {self.name}"
-    
-    def play_card(self, card: Card) -> Optional[Card]:
-        if card in self.hand:
-            self.hand.remove(card)
-            return card
-        else:
-            return None
-
-    def draw_card(self, card: Card):
-        self.hand.append(card)
-
-    def evaluate_hand_for_playable_cards(self, playable_cards: List[Card]):
-        """Choose cards that player can throw on top_stack_card"""
-        self.playable_hand = []
-
-        for card in self.hand:
-            if card in playable_cards:
-                self.playable_hand.append(card)
-
-
-    def make_value_demand(self) -> Value:
-        return random.choice(list(Value))
-
-    def make_suit_demand(self) -> Suit:
-        return random.choice(list(Suit))
-    
 
     def make_move(self) -> Optional[Card]:
 
@@ -108,30 +78,6 @@ class AggressivePlayer(Player):
         super().__init__(name)
         # TODO should I consider queen in this list? # TODO refactor using a dict?
         self.first_cards_to_throw: List[Value] = [Value.ACE, Value.TWO, Value.THREE, Value.FOUR, Value.JACK, Value.QUEEN, Value.KING]
-
-    def __str__(self):
-        return f"Aggressive player: {self.name}"
-
-    def __repr__(self):
-        return f"Aggressive player: {self.name}"
-
-    def draw_card(self, card: Card):
-        self.hand.append(card)
-
-    def evaluate_hand_for_playable_cards(self, playable_cards: List[Card]):
-        """Choose cards that player can throw on top_stack_card"""
-        self.playable_hand = []
-
-        for card in self.hand:
-            if card in playable_cards:
-                self.playable_hand.append(card)
-
-
-    def make_value_demand(self) -> Value:
-        return random.choice(list(Value))
-
-    def make_suit_demand(self) -> Suit:
-        return random.choice(list(Suit))
 
 
     def make_move(self) -> Optional[Card]:
@@ -159,31 +105,6 @@ class CautiousPlayer(Player):
         super().__init__(name)
         self.first_cards_to_throw: List[Value] = [Value.FIVE, Value.SIX, Value.SEVEN, Value.EIGHT, Value.NINE, Value.TEN]
 
-    def __str__(self):
-        return f"Cautious player: {self.name}"
-
-    def __repr__(self):
-        return f"Cautious player: {self.name}"
-
-    def draw_card(self, card: Card):
-        self.hand.append(card)
-
-    def evaluate_hand_for_playable_cards(self, playable_cards: List[Card]):
-        """Choose cards that player can throw on top_stack_card"""
-        self.playable_hand = []
-
-        for card in self.hand:
-            if card in playable_cards:
-                self.playable_hand.append(card)
-
-
-    def make_value_demand(self) -> Value:
-        return random.choice(list(Value))
-
-    def make_suit_demand(self) -> Suit:
-        return random.choice(list(Suit))
-
-
     def make_move(self) -> Optional[Card]:
         """
         Method that implements strategy of a player.
@@ -198,10 +119,32 @@ class CautiousPlayer(Player):
 
         return self.play_card(random.choice(self.playable_hand))
 
+
+class RLPlayer(Player):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self._pending_action: int = 0
+
+    def set_action(self, action: int):
+        self._pending_action = action
+
+    def make_move(self) -> Optional[Card]:
+
+        if not self.playable_hand or self._pending_action >= len(self.playable_hand):
+            return None
+        return self.play_card(self.playable_hand[self._pending_action])
+
+
 class MCTSPlayer(Player):
     """
     Class that implements Monte Carlo Search Trees strategy.
     """
-    pass
+    def __init__(self, name):
+        super().__init__(name)
+        pass
+
+    def make_move(self) -> Optional[Card]:
+        pass
 
 
